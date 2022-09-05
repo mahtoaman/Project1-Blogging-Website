0e9e9ce8895403796899c98319d9dcb7b4028043
mahtoaman <itismyacnt@gmail.com>
2022-09-05 22:34:02 +0530

    changes

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -1,8 +1,10 @@
 const blogModel = require("../models/blogModel");
+const authorModel = require("../models/authorModel");
 
 const createBlog = async function (req, res) {
   try{
   let data = req.body;
+  console.log(data.authorId)
   let authorPresence = await authorModel.findById(data.authorId);
    if (!authorPresence) return res.status(404).send({ status: false, msg: "author is not present" });
 

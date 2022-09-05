68c29cdf538ecf714a07213df9d39ead8b3f5444
mahtoaman <itismyacnt@gmail.com>
2022-09-05 16:34:28 +0530

    just created own app

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const { default: mongoose } = require('mongoose');
+const route = require('./routes/route')
+const app = express();
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+
+mongoose.connect("mongodb+srv://amanmahto:anuragf45@bloggingwebsite.t7a0mo0.mongodb.net/Blogging-Website?retryWrites=true&w=majority",{
+      useNewUrlParser: true,
+    })
+  .then(() => console.log("MongoDb is connected"))
+  .catch((err) => console.log(err));
+
+app.use('/', route)
+
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});

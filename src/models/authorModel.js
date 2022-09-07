0cddbaaace30b04a4627546ca71fbcb5dfadd218
mahtoaman <itismyacnt@gmail.com>
2022-09-07 16:30:53 +0530

    Handling edge cases of authorController

--- src/models/authorModel.js
+++ src/models/authorModel.js
@@ -1,37 +1,32 @@
 const mongoose = require("mongoose");
 
-// let validateEmail = function(email) {
-//   var re = /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/;
-//   return re.test(email)
-// };
-
 const authorSchema = new mongoose.Schema(
   {
     fname: {
       type: String,
-      required: true,
+    //   required: true,
     },
     lname: {
       type: String,
-      required: true,
+    //   required: true,
     },
     title: {
       type: String,
       enum: ["Mr", "Mrs", "Miss"],
-      required: true,
+    //   required: true,
     },
     email: {
       type: String,
-      required: true,
+    //   required: true,
       //unique: true,
       lowercase:true
   },
     password: {
       type: String,
-      required: true,
+    //   required: true,
     },
   },
-  { timestamps: true }
+//   { timestamps: true }
 );
 
 module.exports = mongoose.model("author", authorSchema);
